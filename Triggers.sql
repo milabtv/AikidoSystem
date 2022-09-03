f9c67cfae3c1fcf2b9cbf4d128820294f7b65545
Milosha <milabtv@gmail.com>
2022-09-04 00:47:33 +0300

    Designed database and trigger for kartoteki

---
+++ Triggers.sql
@@ -0,0 +1,19 @@
+﻿CREATE TRIGGER Insert_Kartoteka 
+ON Kartoteki
+INSTEAD OF INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO Kartoteki (EGN,First_name,Middle_name,.Last_name,Telephone,Email_,Address)
+    SELECT
+        i.EGN,i.First_name,i.Middle_name,i.Last_name,i.Telephone,i.Email_,i.Address
+    FROM
+        inserted i
+    WHERE
+        i.EGN NOT IN (
+            SELECT 
+               EGN
+            FROM
+               [Kartoteki]
+        );
+END
